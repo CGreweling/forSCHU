c652d41d334f0aab576d17e0b6b4e1649259985c
CGreweling <cgreweling@uos.de>
2021-05-13 11:22:58 +0200

    Added DHT22, added some exception handling

--- mesureforSCHU.py
+++ mesureforSCHU.py
@@ -1,33 +1,47 @@
+
 #!/usr/bin/env python3
 
 import serial
 import time
 import datetime
 import json
+from influxdb import InfluxDBClient
+
+import time
+import board
+import adafruit_dht
 
 SERIALPORT= "/dev/serial0"
 
 PACKET = [0xFF, 0x01, 0x86, 0x00, 0x00, 0x00, 0x00, 0x00, 0x79]
 
+dhtDevice = adafruit_dht.DHT22(board.D13, use_pulseio=False)
+
 
 def sendToInfluxDB(dataasjson):
-    host = "YOURIPOFINFLUX"
+    host = "YOURHOST"
     port = 8086
     user = 'influxdbuser'
     password = 'YOURPASSWORD'
     dbname = 'forSCHU'
 
-    from influxdb import InfluxDBClient
-    client = InfluxDBClient(host, port, user, password, dbname)
-    entry = [{'measurement': 'CO2',
+    try:
+      client = InfluxDBClient(host, port, user, password, dbname)
+      entry = [{'measurement': 'CO2',
               'tags': {'host': 'MHZ14a'},
               'time': dataasjson['dt'],
               'fields': {
-                  'PPM': float(dataasjson['ppm'])}
+                  'PPM': float(dataasjson['ppm']),
+                  'temp': float(dataasjson['temp']),
+                  'humidity': float(dataasjson['humidity'])
+                 }
               }]
-    ret = client.write_points(entry)
-    # print(entry)
-    print("write_points() to Influxdb on %s db=%s worked=%s  ppm=%s time=%s" % (host, dbname, str(ret), dataasjson['ppm'], dataasjson['dt']))
+      print(entry)
+      ret = client.write_points(entry)
+      # print(entry)
+      print("write_points() to Influxdb on %s db=%s worked=%s  ppm=%s temp=%s humidity=%s time=%s" % (host, dbname, str(ret), dataasjson['ppm'], dataasjson['$
+    except:
+      print("No connection to InfluxDB")
 
 def getData(serialconnection):
     serialconnection.write(bytearray(PACKET))
@@ -53,6 +67,11 @@ def main():
 
        time.sleep(60)
        dataasjson = getData(serialconnection)
+       try:
+         dht22data = getDH22Data()
+         dataasjson.update(dht22data)
+       except:
+         print("could not read dht22")
        sendToInfluxDB(dataasjson)
 
 if __name__ == '__main__':
